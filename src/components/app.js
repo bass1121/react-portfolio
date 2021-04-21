8981b3de4b6e50a35b8dd85a254a99ecfffb6958
seanbass <seanbass99@gmail.com>
2021-04-21 01:18:55 -0500

    third update with created portfolio-detail.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationComponent from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">   
+        
+
+        <Router>
+          <div>
+            <NavigationComponent />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Sean Bass React Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
